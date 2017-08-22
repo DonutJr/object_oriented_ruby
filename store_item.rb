bdcb3cc4598913ddf30d331d41f87dcc59448170
DonutJr <cyrus.younessi@gmail.com>
2017-08-21 21:26:34 -0500

    second day of class

---
+++ store_item.rb
@@ -0,0 +1,15 @@
+# class Subway
+#   def initialize(bread, meat, veggies, seasoning)
+#     @bread = bread
+#     @meat = meat
+#     @veggies = veggies
+#     @seasoning = seasoning
+#   end
+
+# end
+
+sandwich_1 = {bread: "nine grain", meat: "ham", veggies: ["green onions", "lettuce", "tomato", seasoning: "salt and pepper"]}
+sandwich_2 = {:bread => "wheat", :meat => "meatball", :veggies => ["onions, giardinera"], :seasoning => "honey mustard"}
+
+p sandwich_1
+p sandwich_2
